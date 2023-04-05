1b63b3a06db631dcf80613dee660bbf12bc3ecc0
DANIEL <yuntaehui77@gmail.com>
2023-04-05 23:52:11 +0900

    2023.04.05 study at home

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,7 +16,10 @@ public class JpaMain {
         try {
             // 영속
             Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            member.setName("AAAAA");
+
+            em.close();
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("======================");
             tx.commit();
